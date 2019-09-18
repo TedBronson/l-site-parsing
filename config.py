58fbc71de87e88693e5d21170ccee3d1213b0e7d
TedBronson <R3Lz4lraDbZp>
2019-09-18 13:52:33 +0300

    Fixed category ID. Added more offers to DB

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 currencies_list = ['USD']
-search_pages_lower_limit = 21
-search_pages_upper_limit = 30
-category_id = 1600  # categories = {'Houses': 206, 'Apartments': 1600}
+search_pages_lower_limit = 51
+search_pages_upper_limit = 80
+category_id = 1600  # categories = {'Houses': 1602, 'Apartments': 1600}
 db_file_path = "C:\Projects\l-site-parsing\offers.db"
