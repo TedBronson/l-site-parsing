ff4ae85eb45586bac151b46298108fe267cb0273
TedBronson <kabashnyiyaroslav@gmail.com>
2020-12-13 12:54:53 +0200

    Optimized imports, formatted code

--- Data_storage.py
+++ Data_storage.py
@@ -1,26 +1,32 @@
 import sqlite3
+
 from config import db_file_path
 
 
 def write_to_db(offer_string):
     """
-    Appends a new line to a file. No verification and it doesn't update old records.
-    category_id for houses and apartments is hardcoded, so saving other categories would require code adjustments.
+    Appends a new line to a file. No verification and it doesn't update
+    old records.
+    category_id for houses and apartments is hardcoded,
+    so saving other categories would require code adjustments.
     :param offer_string:
     :return:
     """
     from config import category_id
+
     try:
         conn = sqlite3.connect(db_file_path)
         c = conn.cursor()
         if category_id == 1600:
             c.execute(
-                "INSERT INTO offers values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                "INSERT INTO offers values("
+                "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 offer_string,
             )
         elif category_id == 1602:
             c.execute(
-                "INSERT INTO houses values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                "INSERT INTO houses values("
+                "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 offer_string,
             )
         conn.commit()
@@ -48,7 +54,8 @@ def get_parsing_queries():
     """
     Read search queries from DB.
 
-    Function to read and return active queries that should be used to get data about offers with all parameters.
+    Function to read and return active queries that should be used to get data
+    about offers with all parameters.
     :return: list of dictionaries. Where each dictionary is a parsing query
     """
     conn = sqlite3.connect(db_file_path)
@@ -68,4 +75,3 @@ def dict_factory(cursor, row):
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
-
